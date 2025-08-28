da54f71d320fea2f002fdfd5348107e87806f60e
Vassilissa <lissamorys@gmail.com>
2025-08-28 15:30:07 +0700

    menambahkan list di index.php

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos maxime tempora suscipit placeat similique corrupti dicta sequi consequuntur asperiores necessitatibus iure, repudiandae animi modi dolorem natus cumque rem unde enim!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
