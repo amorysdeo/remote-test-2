b31c9b89ae56e871c514098ad524b22d7d0cd9b0
Vassilissa <lissamorys@gmail.com>
2025-08-28 15:37:37 +0700

    membuat file style.css

---
+++ .history/index_20250828152938.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba remote lagi</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos maxime tempora suscipit placeat similique corrupti dicta sequi consequuntur asperiores necessitatibus iure, repudiandae animi modi dolorem natus cumque rem unde enim!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
+</body>
+</html>
